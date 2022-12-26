394328edeec86221ef9120bad1d1adff54f0f612
sanbird <waltz.park@gmail.com>
2022-12-26 10:44:53 +0900

    업데이트01

--- src/exam01/Example.java
+++ src/exam01/Example.java
@@ -11,12 +11,15 @@ public class Example {
 	public static void main(String[] args) {
 		action(new B());
 		action(new C());
+<<<<<<< HEAD
 		
 		System.out.println("새로운 내용");
 		
 		System.out.println("또 새로운 내용");
 		System.out.println("또또 새로운 내용");
 		System.out.println("새새로운 내용");
+=======
+>>>>>>> parent of def2c92 (Example.java 내용 수정)
 
 	}
 
