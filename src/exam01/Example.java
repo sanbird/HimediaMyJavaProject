7cff92daa6e35843962a227d0510e36c5f245e5b
sanbird <waltz.park@gmail.com>
2022-12-26 10:27:20 +0900

    두번째 수정

--- src/exam01/Example.java
+++ src/exam01/Example.java
@@ -4,7 +4,7 @@ public class Example {
 	public static void action(A a) {
 		a.method1();
 		if(a instanceof C c) {
-			c.method2();
+			c.method2(); 
 		}
 	}
 
@@ -13,6 +13,9 @@ public class Example {
 		action(new C());
 		
 		System.out.println("새로운 내용");
+		
+		System.out.println("또 새로운 내용");
+		System.out.println("또또 새로운 내용");
 
 	}
 
